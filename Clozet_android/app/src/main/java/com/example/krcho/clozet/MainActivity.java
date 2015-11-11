3e3df59b82e7b08a33b4487cb7cde43a0ad21dbf
조경래 <notwice92@gmail.com>
2015-11-12 00:51:48 +0900

    now we can use front camera in clozet!!

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.krcho.clozet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.krcho.clozet.camera.FrontCameraActivityLolliPop;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button front = (Button) findViewById(R.id.front_camera);
+        front.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FrontCameraActivityLolliPop.class));
+            }
+        });
     }
 
     @Override
