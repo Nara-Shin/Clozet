babaef00adaeefcbd4ee3669340525d884ac81e9
nara <robotluv1226@naver.com>
2015-11-12 01:26:16 +0900

    작업내용 0. 대충 구조 파악, 주석 조금 1. 요청이 1개 왔을 때와 2개 왔을 경우로 나눔 2.1 요정이 하나일 때 : 팝업 다이얼로그 (해당 상품이 요청되었습니다 출력) -> UI대로 레이아웃 바꿔야함 2.2 요청이 두개일 때 : 팝업 다이얼로그 (상품이 요청되었습니다 출력) -> 리스트로 바꿔야 함

--- Clozet_Manager/app/src/main/java/com/example/krcho/clozetmanager/TestCustomDialogActivity.java
+++ Clozet_Manager/app/src/main/java/com/example/krcho/clozetmanager/TestCustomDialogActivity.java
@@ -3,37 +3,54 @@ package com.example.krcho.clozetmanager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 // popup -> CustomDialog
 import com.example.krcho.clozetmanager.popup.CustomDialog;
 
 public class TestCustomDialogActivity extends Activity {
-
+//1개 신청 시
 	private CustomDialog mCustomDialog;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//팝업다이얼로그로 만들기 (아래 주석)
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
+		//버튼 2개 있는 메인화면
 	}
 
 	public void onClickView(View v){
 		switch (v.getId()) {
-		case R.id.bt_main:
-			mCustomDialog = new CustomDialog(this, 
-					"8월의 크리스마스!!",
-					"영화보러가자~!!!",
-					leftClickListener, 
-					rightClickListener);
-			mCustomDialog.show();
-			break;
+			case R.id.bt_main://1개일 때
+				mCustomDialog = new CustomDialog(this,
+						"",
+						"해당 상품이 요청되었습니다.",
+						leftClickListener,
+						rightClickListener);
+				mCustomDialog.setTitle("X번 룸");
+				mCustomDialog.show();
+				break;
+
+			case R.id.bt_tow://2개 이상일 때
+				mCustomDialog = new CustomDialog(this,
+						"",
+						"상품이 요청되었습니다.",
+						leftClickListener,
+						rightClickListener);
+				mCustomDialog.setTitle("XX번 룸");
+				mCustomDialog.show();
+				break;
+
 		}
 	}
 
 	private View.OnClickListener leftClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), "왼쪽버튼 Click!!", 
+			//수락버튼 누른 경우
+			Toast.makeText(getApplicationContext(), "수락!!",
 					Toast.LENGTH_SHORT).show();
 		}
 	};
@@ -41,7 +58,8 @@ public class TestCustomDialogActivity extends Activity {
 	private View.OnClickListener rightClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), "오른쪽버튼 Click!!", 
+			//거절버튼 누른 경우
+			Toast.makeText(getApplicationContext(), "거절!!",
 					Toast.LENGTH_SHORT).show();
 			mCustomDialog.dismiss();
 		}
