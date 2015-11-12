bce2d64ab14b893bcb7293b73fdc6f8576aa512f
조경래 <notwice92@gmail.com>
2015-11-13 01:42:40 +0900

    롤리팝 및 하위버전 프론트 카메라 보이기 성공. 단점 : 하위버전용 카메라앱은 살짝 반응도 느리고 찌그러지는 경향이 있음... 적절선에서 조절이 필요함

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.krcho.clozet;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.krcho.clozet.camera.FrontCameraActivity;
 import com.example.krcho.clozet.camera.FrontCameraActivityLolliPop;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
         Button front = (Button) findViewById(R.id.front_camera);
         front.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                startActivity(new Intent(getApplicationContext(), FrontCameraActivity.class));
+//                }else {
+//                    startActivity(new Intent(getApplicationContext(), FrontCameraActivityLolliPop.class));
+//                }
+            }
+        });
+
+        Button front_l = (Button) findViewById(R.id.front_camera_l);
+        front_l.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), FrontCameraActivityLolliPop.class));
