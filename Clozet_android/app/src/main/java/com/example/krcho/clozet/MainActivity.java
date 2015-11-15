f859217f6bc3d7baac6d1e761b3056eafd6e9660
조경래 <notwice92@gmail.com>
2015-11-15 21:22:46 +0900

    project base theme and mainActivity

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
@@ -1,60 +1,36 @@
 package com.example.krcho.clozet;
 
-import android.content.Intent;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.example.krcho.clozet.camera.FrontCameraActivity;
-import com.example.krcho.clozet.camera.FrontCameraActivityLolliPop;
-import com.hojung.nfc.HojungNFCCheckingLibrary;
-import com.hojung.nfc.model.NfcModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button front = (Button) findViewById(R.id.front_camera);
-        front.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
-                startActivity(new Intent(getApplicationContext(), FrontCameraActivity.class));
-//                }else {
-//                    startActivity(new Intent(getApplicationContext(), FrontCameraActivityLolliPop.class));
-//                }
-            }
-        });
-
-        Button front_l = (Button) findViewById(R.id.front_camera_l);
-        front_l.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), FrontCameraActivityLolliPop.class));
-            }
-        });
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
     private void initNFC() {
-        HojungNFCCheckingLibrary nfc = new HojungNFCCheckingLibrary(this);
-        NfcModel model = nfc.getNFCData(getIntent());
-        model.getPayloadStr(); // tag에 저장된 string
+//        HojungNFCCheckingLibrary nfc = new HojungNFCCheckingLibrary(this);
+//        NfcModel model = nfc.getNFCData(getIntent());
+//        model.getPayloadStr(); // tag에 저장된 string
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return false;
     }
 
     @Override
