ae54d45727464aac13f1a4053801b5bd1ef67a33
조경래 <notwice92@gmail.com>
2015-11-25 20:37:07 +0900

    unless file delete

--- HojungNFCLibrary/src/com/hojung/nfc/HojungConstants.java
+++
@@ -1,7 +0,0 @@
-package com.hojung.nfc;
-
-public class HojungConstants {
-
-	public static String NFC_PERMISSION="android.permission.NFC";
-	public static String BIND_NFC_SERVICE_PERMISSION="android.permission.BIND_NFC_SERVICE";
-}
