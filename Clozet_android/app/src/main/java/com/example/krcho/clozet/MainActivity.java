1e7dbd4a61fa3ef2645074c2dc6b8b9623bb0754
조경래 <notwice92@gmail.com>
2015-11-26 01:29:30 +0900

    mainActivity에서 NFC 연동 (읽기 가능)

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
@@ -6,20 +6,28 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.krcho.clozet.barcode.BarcodeDetactActivity;
 import com.example.krcho.clozet.camera.FrontCameraActivity;
 import com.example.krcho.clozet.camera.FrontCameraActivityLolliPop;
+import com.hojung.nfc.HojungNFCReadLibrary;
+import com.hojung.nfc.interfaces.OnHojungNFCListener;
+import com.hojung.nfc.model.NfcModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ViewPager viewPager;
     private TextView change, shot;
+
+    //NFC
+    HojungNFCReadLibrary hojungNFCReadLibrary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,25 +50,45 @@ public class MainActivity extends AppCompatActivity {
         shot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
                     startActivity(new Intent(getApplicationContext(), FrontCameraActivity.class));
-                }else {
+                } else {
                     startActivity(new Intent(getApplicationContext(), FrontCameraActivityLolliPop.class));
                 }
 
             }
         });
 
+        //NFC
+
+        hojungNFCReadLibrary = new HojungNFCReadLibrary(getIntent(), MainActivity.this, new OnHojungNFCListener() {
+
+            @Override
+            public void onReceiveMessage(NfcModel[] models) {
+                // TODO Auto-generated method stub
+                //0��° �� ���
+                // model- Ÿ�԰� ���̷ε�  �ƹ��ų� ��� ����(Write �ۿ��� ���� ����� �ν� ����.)
+                Toast.makeText(MainActivity.this, "type : " + models[0].getTypeStr() + " , " + "payload : " + models[0].getPayloadStr(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(String arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
     }
 
-    public void setViewPager(){
+    public void setViewPager() {
 
     }
 
     private void initNFC() {
-//        HojungNFCCheckingLibrary nfc = new HojungNFCCheckingLibrary(this);
-//        NfcModel model = nfc.getNFCData(getIntent());
-//        model.getPayloadStr(); // tag에 저장된 string
+        Log.d("NFC", "intent : " + getIntent().getAction());
+        Intent intent = getIntent();
+        hojungNFCReadLibrary.onResume(intent);
+
     }
 
     @Override
@@ -75,6 +103,21 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         initNFC();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("NFC", "onPause");
+        hojungNFCReadLibrary.onPause();
+    }
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d("NFC", "onNewIntent");
+        hojungNFCReadLibrary.onNewIntent(intent);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
