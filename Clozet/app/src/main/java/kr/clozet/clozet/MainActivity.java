1995814d108e0219dbbde7c24d75c65d45bc45f3
안서형 <ash989848@gmail.com>
2015-11-27 11:16:10 +0900

    카메라 전환 추가

--- Clozet/app/src/main/java/kr/clozet/clozet/MainActivity.java
+++ Clozet/app/src/main/java/kr/clozet/clozet/MainActivity.java
@@ -1,15 +1,7 @@
 package kr.clozet.clozet;
 
-import android.content.Intent;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -22,17 +14,17 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
 
     private int mCameraId = -1;
     private ZXingScannerView mScannerView;
+    private boolean frontCamera = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        CoordinatorLayout Clayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
-        Snackbar.make(Clayout, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+//        CoordinatorLayout Clayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
+//        Snackbar.make(Clayout, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
 
         mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
         setContentView(mScannerView);                // Set the scanner view as the content view
@@ -73,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -84,10 +77,26 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_camera) {
+            if (frontCamera) {
+                frontCamera = false;
+                changeCamera();
+            } else {
+                frontCamera = true;
+                changeCamera();
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeCamera() {
+        mScannerView.stopCamera();
+
+        if (frontCamera) {
+            mScannerView.startCamera(1);
+        } else {
+            mScannerView.startCamera(0);
+        }
     }
 }
