1fe29cc662ec18aeb95bdcfd974d56818fd43c5e
krmpr <notwice92@gmail.com>
2015-12-27 14:37:05 +0900

    gcm 붙임

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.krcho.clozet;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +19,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.krcho.clozet.barcode.BarcodeDetactActivity;
 import com.example.krcho.clozet.camera.CameraGuideActivity;
+import com.example.krcho.clozet.gcm.QuickstartPreferences;
+import com.example.krcho.clozet.gcm.RegistrationIntentService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.hojung.nfc.HojungNFCReadLibrary;
 import com.hojung.nfc.interfaces.OnHojungNFCListener;
 import com.hojung.nfc.model.NfcModel;
@@ -33,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
     HojungNFCReadLibrary hojungNFCReadLibrary;
 
     Context mContext;
+
+    //GCM
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,7 +128,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //GCM
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+            }
+        };
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+
     }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
 
     public void setViewPager() {
 
@@ -137,13 +188,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         initNFC();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
     }
 
     @Override
     protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
         super.onPause();
         Log.d("NFC", "onPause");
         hojungNFCReadLibrary.onPause();
+
     }
 
 
