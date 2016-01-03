0e829ea37e3ae69e8b3dcc63a77150137ee39626
hojung <topchoco@naver.com>
2016-01-03 20:33:31 +0900

    프로필 화면 수정

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/MainSideView.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/MainSideView.java
@@ -16,9 +16,7 @@ public class MainSideView extends LinearLayout implements View.OnClickListener {
 
 
     private Context context;
-    private FrameLayout frameLayout_sideBar_rightside;
     private ImageView imageView_sidebar_logo,imageView_sidebar_promotion,imageView_sidebar_gallery,imageView_sidebar_setting,imageView_sidebar_cancel;
-   private boolean logoClicked=false,promotionClicked=false,galleryClicked=false,settingClicked=false,cancelClicked=false;
     public MainSideView(Context context) {
 
         super(context);
@@ -33,13 +31,13 @@ public class MainSideView extends LinearLayout implements View.OnClickListener {
     public void init(Context context){
         this.context=context;
         LayoutInflater inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.view_main_sideBar, this, true);
-        frameLayout_sideBar_rightside=(FrameLayout)findViewById(R.id.frameLayout_sideBar_rightside);
+        inflater.inflate(R.layout.view_main_sidebar, this, true);
         imageView_sidebar_promotion=(ImageView)findViewById(R.id.imageView_sidebar_promotion);
         imageView_sidebar_gallery=(ImageView)findViewById(R.id.imageView_sidebar_gallery);
         imageView_sidebar_logo=(ImageView)findViewById(R.id.imageView_sidebar_logo);
         imageView_sidebar_setting=(ImageView)findViewById(R.id.imageView_sidebar_setting);
         imageView_sidebar_cancel=(ImageView)findViewById(R.id.imageView_sidebar_cancel);
+        this.bringToFront();
 
 
 
@@ -72,8 +70,6 @@ public class MainSideView extends LinearLayout implements View.OnClickListener {
             imageView_sidebar_gallery.setImageResource(R.drawable.btn_gallery_unclick);
 
         }else if(view_id==imageView_sidebar_logo.getId()){
-
-        }else if(view_id==frameLayout_sideBar_rightside.getId()){
 
         }
 
