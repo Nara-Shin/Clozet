6a6e676f7fba2457790f66d68c0f04082a9dce49
krmpr <notwice92@gmail.com>
2016-01-06 12:50:23 +0900

    변경된 서버주소 반영

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/network/NetDefine.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/network/NetDefine.java
@@ -4,7 +4,7 @@ package com.example.krcho.clozet.network;
  * Created by krcho on 2015-11-26.
  */
 public interface NetDefine {
-    String BASE_URL = "http://125.209.199.91/clozet/";
+    String BASE_URL = "http://godeung.woobi.co.kr/clozet/";
     String SEARCH_BARCODE = "search/product.php";
     String JOIN_SERVICE = "member/join.php";
 }
