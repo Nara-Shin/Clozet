1ab148b05a076ed51f387fee00e66c111938b75f
안서형 <ash989848@gmail.com>
2016-01-08 17:13:34 +0900

    [160108] 착샷 촬영 구현, 미리보기 구현 중

---
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/camera/CameraPreviewActivity.java
@@ -0,0 +1,18 @@
+package com.example.krcho.clozet.camera;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.krcho.clozet.R;
+
+public class CameraPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_preview);
+
+
+    }
+
+}
