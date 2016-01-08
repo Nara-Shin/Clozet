e15e86197060a9dcb5cb82581c2b6e9ca43971de
안서형 <ash989848@gmail.com>
2016-01-08 20:34:30 +0900

    [160108] 착샷 촬영 후 미리보기 작업 중

--- Clozet_android/app/src/main/java/com/example/krcho/clozet/camera/CameraPreviewActivity.java
+++ Clozet_android/app/src/main/java/com/example/krcho/clozet/camera/CameraPreviewActivity.java
@@ -1,18 +1,24 @@
 package com.example.krcho.clozet.camera;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.krcho.clozet.R;
 
+import java.io.File;
+
 public class CameraPreviewActivity extends AppCompatActivity {
+
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_preview);
 
-
+//        Intent intent = getIntent();
+//        file = intent.getExtras().getFi
     }
 
 }
